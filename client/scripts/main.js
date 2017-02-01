4747f6705f0019c718ae2856b338e2704026edbe
Fábio Maia <fabiu.maia@gmail.com>
2017-02-01 21:51:19 +0000

    Refactor Cleanup Improve styling Parse response into dictionary Check if a good meal exists

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -4,19 +4,80 @@ function getJSON(url, cb) {
 
     request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
-            const response = JSON.parse(request.responseText)
-            console.log(response)
-            cb(response)
+            cb(JSON.parse(request.responseText))
         }
     }
 
     request.send()
 }
 
-function updateAnswer(response) {
-    const answer = document.querySelector('#answer')
-    answer.textContent = 'Sim'
+function getJSONP(url, param, cb) {
+    const script = document.createElement('script')
+    script.src = `${url}&${param}=${cb.name}`
+    document.querySelector('head').appendChild(script)
 }
 
-const url = 'http://0.0.0.0:1337/api'
-getJSON(url, updateAnswer)
+function isInterestedInDinner() {
+    return new Date().getHours() >= 14
+}
+
+function validMenus(menus) {
+    return menus.filter(m => m.items.item != {} && !m['@attributes'].disabled.startsWith('Encerrado'))
+}
+
+function lunchMenus(menus) {
+    return menus.filter(m => m['@attributes']['meal'] == 'Almoço')
+}
+
+function dinnerMenus(menus) {
+    return menus.filter(m => m['@attributes']['meal'] == 'Jantar')
+}
+
+function parseResponse(response) {
+    menus = validMenus(response.menus.menu)
+    menus = isInterestedInDinner() ? lunchMenus(menus) : dinnerMenus(menus)
+
+    const map = new Map()
+
+    for (const menu of menus) {
+        map.set(menu['@attributes']['canteen'], menu.items.item.filter(e => typeof e === 'string'))
+    }
+
+    return map
+}
+
+function updateAnswer(response) {
+    const menus = parseResponse(response)
+    const best = bestMenu(menus)
+
+    if (best) {
+        const answer = document.querySelector('#answer')
+        answer.textContent = 'Sim.'
+        answer.classList.add('positive')
+
+        const canteen = document.querySelector('#canteen')
+        canteen.textContent = `ao ${best.canteen}`
+
+        const meal = document.querySelector('#meal')
+        meal.textContent = `${best.meal.toLowerCase()}.`
+    }
+}
+
+function bestMenu(menus) {
+    const good = ['frango', 'lombo', 'bacalhau com natas', 'peru']
+
+    for (const [canteen, meals] of menus) {
+        for (const meal of meals) {
+            for (const g of good) {
+                if (meal.toLowerCase().includes(g)) {
+                    return {canteen, meal}
+                }
+            }
+        }
+    }
+
+    return null
+}
+
+const url = 'http://services.web.ua.pt/sas/ementas?date=day&format=jsonp'
+getJSONP(url, 'cb', updateAnswer)
